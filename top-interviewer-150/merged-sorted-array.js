96ddcd8a9910986f8631b8d5d23f2b52f2910e62
Matheus Ednei <ednei880@gmail.com>
2024-11-20 16:26:28 -0300

    refactor(exercise): refactor merged sorted array exercise

--- top-interviewer-150/merged-sorted-array.js
+++ top-interviewer-150/merged-sorted-array.js
@@ -6,29 +6,17 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let length = m + n
-    let removed = false;
-    let count = 0;
-    for (let i = 0; i < length; i++) {
+    for (let i = (m + n); i >= 0; i--) {
+        if (nums1[i] === 0 && i >= m) {
+            nums1.splice(i, 1);
+        }
         if (i < n) nums1.push(nums2[i]);
     }
-    
-    for (let i = 0; i < length; i++) {
-        if (removed) {
-            if (nums1[i-count] === 0) {
-                nums1.splice(i-count,1);
-                count++;
-            }
-        } else {
-            if (nums1[i] === 0) {
-                nums1.splice(i,1);
-                removed = true;
-                count++;
-            }   
-        }
-    }
-    
-    nums1.sort();
+    console.log(nums1);
+    nums1.sort(function (a, b) {
+        return a - b;
+    });
+    console.log(nums1);
 };
 
-merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+merge([-1,0,0,3,3,3,0,0,0], 6, [1,2,2], 3)
